ebbd70214b9e6c19a007dc03a91ac3f928e90cd1
Mykyta Konopelko <nik.sativa@gmail.com>
2024-08-03 16:30:54 +0200

    fix UTest

--- Tests/Validator/TextValidator_NotEmptyTests.swift
+++ Tests/Validator/TextValidator_NotEmptyTests.swift
@@ -5,7 +5,7 @@ final class TextValidator_NotEmptyTests: XCTestCase {
     func test_spec() {
         let subject: TextValidator = .notEmpty(errorText: "error")
 
-        XCTAssertEqual(subject.validate(""), [.init(errorText: "error", isValid: false)])
+        XCTAssertEqual(subject.validate(""), [.init(errorText: .plain("error"), isValid: false)])
         XCTAssertEqual(subject.validate("some text"), .valid)
     }
 }
